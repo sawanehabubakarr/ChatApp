1d64ea650145437ebe55cc67c2cf7be0aab6b4b5
sawanehabubakarr <sawanehabubakarr1@gmail.com>
2024-05-08 20:11:20 +0000

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,17 @@
+const config = {
+    apiKey: "AIzaSyAXlsAF_kxrHbjgHSKhyXy6rXYpo4hRDko",
+    authDomain: "javascript-chatapp-77fdc.firebaseapp.com",
+    databaseURL: "https://javascript-chatapp-77fdc-default-rtdb.firebaseio.com",
+    projectId: "javascript-chatapp-77fdc",
+    storageBucket: "javascript-chatapp-77fdc.appspot.com",
+    messagingSenderId: "1016502598123",
+    appId: "1:1016502598123:web:058f5e22cfabc556d541ae",
+    measurementId: "G-ZYDVHGMZ0H",
+    CometChatAppId: "257360074b9637d1",
+    CometChatRegion: "eu",
+    CometChatAuthKey: "7689c1c0d7c05f4038f72a5df88892d8b8e190ad",
+    CometChatAPIKey: "0806ff547c064488daaa59ad8f19460f3d7802ab",
+    CometChatWidgetId: "8d7cb4ae-fd85-4f3d-bc30-e27f0ff7755e"
+  };
+
+  
